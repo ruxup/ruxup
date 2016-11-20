837b9f4ecb6d12824a5cab9ac79380c15affd995
radualex <radu.stoica1994@gmail.com>
2016-11-20 11:01:47 +0100

    Added nationality to user

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('nationality')->nullable();
             $table->text('bio')->nullable();
             $table->string('city')->nullable();
             $table->string('profile_pic')->nullable();
