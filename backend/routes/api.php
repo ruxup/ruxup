a4474ff3f3ebf80e6b3fdc97e687270062a52184
radualex <radu.stoica1994@gmail.com>
2016-11-30 15:53:19 +0100

    update cors again

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-header('Access-Control-Allow-Origin:'. $_SERVER['HTTP_ORIGIN']);
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers:' . 'accept, content-type,x-xsrf-token, x-csrf-token');
 header('Access-Control-Allow-Methods:' . 'GET, POST, PUT, DELETE, OPTIONS');
 
